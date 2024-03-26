27ad05fc0d84e38f5290ecf0afadfad3b774a465
Minhyung Cho <126141140+alsgud8311@users.noreply.github.com>
2024-03-26 19:57:47 +0900

    [Silver II] Title: 순열 선물하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/30021. 순열 선물하기/순열 선물하기.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+if n== 1:
+    print('YES')
+    print(1)
+elif n == 2:
+    print('NO')
+elif n == 3:
+    print('YES')
+    print(1, 3, 2)
+else:
+    print('YES')
+    print(1, 3, 2, end=' ')
+    for i in range(4, n+1):
+        print(i, end=' ')
