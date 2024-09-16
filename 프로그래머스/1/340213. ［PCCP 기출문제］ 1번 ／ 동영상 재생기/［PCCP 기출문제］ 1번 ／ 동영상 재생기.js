789c465396f2e7518dbf66b4866b4db3edf9056e
Minhyung Cho <126141140+alsgud8311@users.noreply.github.com>
2024-09-17 01:21:43 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -1,119 +1,42 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
 function solution(video_len, pos, op_start, op_end, commands) {
-    let cur_s = convertToSec(pos)
-    const end_s = convertToSec(video_len)
-
-    if(is_inside_op(cur_s, op_start, op_end)){
-        cur_s = convertToSec(op_end)
+    let curr = convertToSecond(pos);
+    const opStart = convertToSecond(op_start);
+    const opEnd = convertToSecond(op_end);
+    const full = convertToSecond(video_len);
+    const commandList = {
+        next: () => curr = next(opStart,opEnd,curr,full),
+        prev: () => curr = curr-10 < 0 ? 0 : curr-10
     }
-
-    commands.forEach(command=>{
-        if(command === 'next'){
-            cur_s = next(cur_s, end_s)
-        }else{
-            cur_s = prev(cur_s, end_s)
-        }
-        if(is_inside_op(cur_s, op_start, op_end)){
-           cur_s = convertToSec(op_end)
-        }
+    
+    curr = checkOpening(opStart,opEnd,curr);
+    commands.forEach((command) => {
+        commandList[command]();
+        curr = checkOpening(opStart,opEnd,curr);
     })
-
-
-    return convertToFormatString(cur_s);
+    return convertToTime(curr);
 }
 
-function next(cur_sec, end_sec){
-    const result = cur_sec + 10
-
-    if(result >= end_sec){
-        return end_sec
+function checkOpening(op_start,op_end,curr){
+    if(op_start <= curr && op_end >= curr){
+        return op_end;
     }
-    return result
+    return curr;
 }
 
-function prev(cur_sec){
-    const result = cur_sec - 10
-    if(result < 0){
-        return 0
-    }
-    return result
+function next(op_start,op_end,curr,full){
+    curr += 10;
+    if (full <= curr) return full;
+    return curr;
 }
 
-function is_inside_op(cur_sec, op_start, op_end){
-    const ops = convertToSec(op_start)
-    const opend = convertToSec(op_end)
-    return ops<= cur_sec && cur_sec <= opend
+
+const convertToSecond = (time) => {
+    [min,sec] = time.split(":").map((t)=>parseInt(t));
+    return 60*min + sec;
 }
 
-function convertToSec(formatString){
-    const [m,s] = formatString.split(':').map(Number)
-
-    return 60*m+s
-}
-
-function convertToFormatString(sec){
+const convertToTime = (sec) => {
     const m = String(Math.floor(sec / 60)).padStart(2,"0")
     const s = String(sec % 60).padStart(2,"0")
-
-    return m + ':' + s
+    return `${m}:${s}`
 }
