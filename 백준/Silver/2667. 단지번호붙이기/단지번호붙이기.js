66fa0a7c6af49b19b723be0dde6b2440659b6bfe
Minhyung Cho <126141140+alsgud8311@users.noreply.github.com>
2025-02-10 10:56:39 +0900

    [Silver I] Title: 단지번호붙이기, Time: 96 ms, Memory: 10160 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -12,23 +12,20 @@ const dLoc = [
   [1, 0],
   [0, 1],
 ];
-let houses = 0;
 const result = [];
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
     if (houseMap[i][j]) {
-      dfs(i, j);
-      result.push(houses);
-      houses = 0;
+      result.push(bfs(i, j));
     }
   }
 }
 console.log(result.length);
 console.log(result.sort((a, b) => a - b).join("\n"));
 
-function dfs(x, y, count = 0) {
+function dfs(x, y) {
   houseMap[x][y] = 0;
-  houses++;
+  let count = 1;
   for (const [dx, dy] of dLoc) {
     if (
       x + dx >= 0 &&
@@ -37,7 +34,28 @@ function dfs(x, y, count = 0) {
       y + dy < N &&
       houseMap[x + dx][y + dy]
     ) {
-      dfs(x + dx, y + dy, count + 1);
+      count += dfs(x + dx, y + dy);
     }
   }
+  return count;
+}
+
+function bfs(x, y) {
+  let count = 1;
+  const queue = [[x, y]];
+  houseMap[x][y] = 0;
+
+  while (queue.length) {
+    const [cx, cy] = queue.shift();
+    for (const [dx, dy] of dLoc) {
+      const nx = cx + dx,
+        ny = cy + dy;
+      if (nx >= 0 && nx < N && ny >= 0 && ny < N && houseMap[nx][ny]) {
+        houseMap[nx][ny] = 0;
+        queue.push([nx, ny]);
+        count++;
+      }
+    }
+  }
+  return count;
 }
