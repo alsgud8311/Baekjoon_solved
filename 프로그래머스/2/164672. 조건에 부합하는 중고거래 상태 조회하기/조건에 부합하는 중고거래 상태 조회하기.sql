7f062af07115047cb37ef775200c249b88ad2e53
Minhyung Cho <126141140+alsgud8311@users.noreply.github.com>
2025-02-13 16:18:16 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,4 @@
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, (case when STATUS="DONE" then "거래완료" when STATUS="RESERVED" then "예약중" else "판매중" end) as STATUS
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE like "%2022-10-05%"
+order by BOARD_ID DESC
