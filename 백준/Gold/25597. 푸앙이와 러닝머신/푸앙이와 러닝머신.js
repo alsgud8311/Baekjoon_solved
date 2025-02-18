e60864184fed762e40df1549dc428d9cafcef5a1
Minhyung Cho <126141140+alsgud8311@users.noreply.github.com>
2025-02-18 20:43:38 +0900

    [Gold IV] Title: 푸앙이와 러닝머신, Time: 96 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Gold/25597. 푸앙이와 러닝머신/푸앙이와 러닝머신.js
+++ 백준/Gold/25597. 푸앙이와 러닝머신/푸앙이와 러닝머신.js
@@ -9,54 +9,44 @@ let [n, k] = require("fs")
 let res8 = 0,
   res4 = 0,
   res1 = 0;
-
-// 최대한 8을 먼저 사용하는 전략
 if (n >= 8) {
   res8 = Math.floor(n / 8);
   n %= 8;
 }
 
-// 나머지에서 4를 우선 사용
 if (n >= 4) {
   res4 = Math.floor(n / 4);
   n %= 4;
 }
 
-// 나머지는 1로 채우기
 res1 = n;
 
-let sum = res1 + res4 + res8;
-if (sum > k) {
-  console.log("-1");
-  return;
+let timeSum = res8 + res4 + res1;
+if (timeSum > k) {
+  console.log(-1);
+  process.exit(0);
 }
 
-if (sum < k) {
-  // k와 차이가 있을 경우 추가 조정
-  if (res8 > 0 && k - sum >= res8) {
+if (timeSum < k) {
+  if (res8 > 0 && k - timeSum >= res8) {
     res4 += res8 * 2;
     res8 = 0;
-    sum = res4 + res1 + res8;
+    timeSum = res4 + res1 + res8;
   }
-
-  if (res4 > 0 && k - sum >= res4 * 3) {
+  if (res4 > 0 && k - timeSum >= res4 * 3) {
     res1 += res4 * 4;
     res4 = 0;
-    sum = res1 + res4 + res8;
+    timeSum = res1 + res4 + res8;
   }
 }
-
-// 버튼 결과 배열을 준비
-const vec = [];
-if (res8 > 0) vec.push([8, res8]);
-if (res4 > 0) vec.push([4, res4]);
-if (res1 > 0) vec.push([1, res1]);
-
-console.log(vec.length);
-console.log(`${k - sum} ${vec[0][0]}`);
-
-let time = k - sum + vec[0][1];
-for (let i = 1; i < vec.length; i++) {
-  console.log(`${time} ${vec[i][0]}`);
-  time += vec[i][1];
+const result = [];
+if (res8 > 0) result.push([8, res8]);
+if (res4 > 0) result.push([4, res4]);
+if (res1 > 0) result.push([1, res1]);
+console.log(result.length);
+console.log(`${k - timeSum} ${result[0][0]}`);
+let currTime = k - timeSum + result[0][1];
+for (let i = 1; i < result.length; i++) {
+  console.log(`${currTime} ${result[i][0]}`);
+  currTime += result[i][1];
 }
