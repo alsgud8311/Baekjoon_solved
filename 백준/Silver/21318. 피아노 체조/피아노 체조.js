71dcbde0753742f69d5c6a9f339593f20d1ffc0f
Minhyung Cho <126141140+alsgud8311@users.noreply.github.com>
2025-02-27 11:37:53 +0900

    [Silver I] Title: 피아노 체조, Time: 388 ms, Memory: 70584 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.js
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.js
@@ -8,17 +8,14 @@ const inputs = require("fs")
 const n = +inputs[0];
 const sheet = [0, ...inputs[1].split(" ").map(Number)];
 const questions = inputs.slice(3).map((q) => q.split(" ").map(Number));
-
 const dp = new Array(n + 1).fill(0);
 
 for (let i = 1; i < n; i++) {
   dp[i] = dp[i - 1] + (sheet[i] > sheet[i + 1] ? 1 : 0);
 }
 
-// 🔹 O(1)로 답을 출력
 let result = "";
 for (const [x, y] of questions) {
   result += `${dp[y - 1] - dp[x - 1]}\n`;
 }
-
 console.log(result);
