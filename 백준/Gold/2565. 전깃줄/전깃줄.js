9a0e678a70f202ca1151863f2193a73257f4fcd7
Minhyung Cho <126141140+alsgud8311@users.noreply.github.com>
2025-03-07 11:25:46 +0900

    [Gold V] Title: 전깃줄, Time: 88 ms, Memory: 9400 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.js
+++ 백준/Gold/2565. 전깃줄/전깃줄.js
@@ -8,11 +8,16 @@ const dp = new Array(100).fill(1);
 const wires = w.map((w) => w.split(" ").map(Number));
 wires.sort((a, b) => a[0] - b[0]);
 
-for (let i = 1; i < n; i++) {
-  for (let j = 0; j < i; j++) {
-    if (wires[i][1] > wires[j][1]) {
-      dp[i] = Math.max(dp[i], dp[j] + 1);
-    }
+const lis = [];
+for (const [, b] of wires) {
+  let left = 0,
+    right = lis.length;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (lis[mid] >= b) right = mid;
+    else left = mid + 1;
   }
+  if (left < lis.length) lis[left] = b;
+  else lis.push(b);
 }
-console.log(n - Math.max(...dp));
+console.log(n - lis.length);
