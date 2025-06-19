0aae88fdbdd68ae53810c165096dd33c7c47b2cd
Minhyung Cho <126141140+alsgud8311@users.noreply.github.com>
2025-06-19 11:03:58 +0900

    [Silver I] Title: 숨바꼭질, Time: 3412 ms, Memory: 20288 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,31 +1,33 @@
-const fs = require("fs");
-
 const INPUT_FILE = process.platform === "linux" ? "/dev/stdin" : "./inputs.txt";
-const [n, k] = fs
+const [n, k] = require("fs")
   .readFileSync(INPUT_FILE)
   .toString()
   .trim()
   .split(" ")
   .map(Number);
-
-const MAX = Math.max(n, k) * 2 + 1;
-const visited = new Array(MAX).fill(false);
-const queue = [[n, 0]];
-
+const visited = new Array(100000).fill(false);
 visited[n] = true;
+const queue = [[n, 0]];
+let min = Infinity;
 
 while (queue.length) {
-  const [to, cnt] = queue.shift();
-
-  if (to === k) {
-    console.log(cnt);
+  const [loc, time] = queue.shift();
+  if (loc === k) {
+    min = Math.min(min, time);
     break;
   }
-
-  for (const next of [to + 1, to - 1, to * 2]) {
-    if (next >= 0 && next < MAX && !visited[next]) {
-      visited[next] = true;
-      queue.push([next, cnt + 1]);
-    }
+  if (loc + 1 <= 100000 && !visited[loc + 1]) {
+    visited[loc + 1] = true;
+    queue.push([loc + 1, time + 1]);
+  }
+  if (loc - 1 >= 0 && !visited[loc - 1]) {
+    visited[loc - 1] = true;
+    queue.push([loc - 1, time + 1]);
+  }
+  if (loc * 2 <= 100000 && !visited[loc * 2]) {
+    visited[loc * 2] = true;
+    queue.push([loc * 2, time + 1]);
   }
 }
+
+console.log(min);
